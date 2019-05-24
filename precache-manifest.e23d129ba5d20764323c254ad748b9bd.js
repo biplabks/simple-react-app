abb30a175594a746d9f277572a8317edd9fdebfa
Biplab Kumar Saha <bjbs.anjoy@gmail.com>
2019-05-23 19:43:42 -0500

    initial commit

---
+++ precache-manifest.e23d129ba5d20764323c254ad748b9bd.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "57a5383080fa7628fb04",
+    "url": "/simple-react-app/static/css/main.5f01ab2d.chunk.css"
+  },
+  {
+    "revision": "57a5383080fa7628fb04",
+    "url": "/simple-react-app/static/js/main.490e31e3.chunk.js"
+  },
+  {
+    "revision": "6fd64f1acbc6de00f5ea",
+    "url": "/simple-react-app/static/js/runtime~main.d5660f83.js"
+  },
+  {
+    "revision": "631cd1b0cd6b8b6b3fb4",
+    "url": "/simple-react-app/static/js/2.017d1bc3.chunk.js"
+  },
+  {
+    "revision": "4c3fb584d3f63821357c8a9cff97f360",
+    "url": "/simple-react-app/static/media/haymaker-webfont.4c3fb584.eot"
+  },
+  {
+    "revision": "d2f8799a13e6db2022d9a7de22776705",
+    "url": "/simple-react-app/static/media/anchor.d2f8799a.svg"
+  },
+  {
+    "revision": "cf798e30eda73d84b25c4ae233fe5e21",
+    "url": "/simple-react-app/static/media/haymaker-webfont.cf798e30.svg"
+  },
+  {
+    "revision": "e9535f20ca175cc3fdfe2816f7c47168",
+    "url": "/simple-react-app/static/media/haymaker-webfont.e9535f20.ttf"
+  },
+  {
+    "revision": "73a576a8254aeafebcdc66fe9fd41ac8",
+    "url": "/simple-react-app/static/media/blanch_caps_inline-webfont.73a576a8.eot"
+  },
+  {
+    "revision": "1461f8e9d2084c70c00830348efe1766",
+    "url": "/simple-react-app/static/media/blanch_caps_inline-webfont.1461f8e9.woff"
+  },
+  {
+    "revision": "751e56274d3ea1c651ea824f902bb987",
+    "url": "/simple-react-app/static/media/haymaker-webfont.751e5627.woff"
+  },
+  {
+    "revision": "8345e1b3362c1d640f0f11b5aae1d5bc",
+    "url": "/simple-react-app/static/media/blanch_caps_inline-webfont.8345e1b3.ttf"
+  },
+  {
+    "revision": "7bbffda6bcacac8360f0ee00bb03dc04",
+    "url": "/simple-react-app/static/media/blanch_caps_inline-webfont.7bbffda6.svg"
+  },
+  {
+    "revision": "aacd7cec01d09076d9395dd5fcc2ec15",
+    "url": "/simple-react-app/index.html"
+  }
+];
